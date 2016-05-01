eafab836e55c51ef233cda0b6207b789f2eeca76
Joni Selmes <hollycarter@Jonis-MacBook-Pro.local>
2016-05-01 14:17:32 -0700

    initial setup of website, added html, css, and js to files

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('default', ['uglify','sass' ,'browser-sync']);
+
+
+gulp.task('browser-sync', function() {
+
+    browserSync.init({
+        open: false,
+        proxy: 'http://192.168.33.10/project2'
+      });
+
+      // Watch Tasks
+      gulp.watch('./src/*.js',['uglify']);
+      gulp.watch('./src/*.scss',['sass']);
+
+      gulp.watch(['./build/**/*.*', 'index.html']).on('change', browserSync.reload);
+});
+
+gulp.task('sass', function(){
+  return gulp.src('./src/*.scss')
+     .pipe(sass().on('error', sass.logError))
+     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('uglify', function(){
+ gulp.src('./src/*.js') // What files do we want gulp to consume?
+     .pipe(uglify()) // Call the uglify function on these files
+     .pipe(gulp.dest('./build')) // Where do we put the result?
+
+});
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// $(function() {
+//   $('select').on('change', function() {
+//
+//     $.ajax({
+//
+//       method: 'GET',
+//       url: 'http://api.nytimes.com/svc/topstories/v1/world.json?api-key=766c9f7c67d153d7f4ae5f1861604d43:8:75124094'
+// })
+// .done(function)
+// });
+// });
