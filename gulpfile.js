78ceb258c98c4359af86dda038f5fc2541be5970
Holly Carter <hollycarter@Jonis-MacBook-Pro.local>
2016-05-02 19:16:26 -0700

    organized my scss files, added javascript to adjust selection of articles.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-gulp.task('default', ['uglify','sass','browser-sync']);
+gulp.task('default', ['uglify', 'browser-sync']);
 
 
 gulp.task('browser-sync', function() {
@@ -14,20 +14,20 @@ gulp.task('browser-sync', function() {
       });
 
       // Watch Tasks
-      gulp.watch('./src/*.js',['uglify']);
-      gulp.watch('./src/*.scss',['sass']);
+      gulp.watch('./src/**/*.js',['uglify']);
+      gulp.watch('./src/**/*.scss',['sass']);
 
       gulp.watch(['./build/**/*.*', 'index.html']).on('change', browserSync.reload);
 });
 
 gulp.task('sass', function(){
-  return gulp.src('./src/*.scss')
-     .pipe(sass().on('error', sass.logError))
+  return gulp.src('./src/**/*.scss')
+     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('uglify', function(){
- gulp.src('./src/*.js') // What files do we want gulp to consume?
+ gulp.src('./src/**/*.js') // What files do we want gulp to consume?
      .pipe(uglify()) // Call the uglify function on these files
      .pipe(gulp.dest('./build')); // Where do we put the result?
 
--- src/main.js
+++ src/main.js
@@ -1,29 +1,39 @@
 $(function() {
-
-
   $('select').on('change', function() {
 
-    $('img').switchClass("logolarge", "logosmall", 1000, "easeOutBounce");
+    $('header').switchClass("headlarge", "headsmall", 1000, "easeOutQuad");
+    $('img').switchClass("logolarge", "logosmall", 1000, "easeOutQuad");
+
+    $(".all-categories").empty();
+    $('.loading').show();
+
+    var select = $('.selection').val();
     $.ajax({
 
       method: 'GET',
-      url: 'http://api.nytimes.com/svc/topstories/v1/world.json?api-key=766c9f7c67d153d7f4ae5f1861604d43:8:75124094'
+      url: 'http://api.nytimes.com/svc/topstories/v1/' + select + '.json?api-key=766c9f7c67d153d7f4ae5f1861604d43:8:75124094'
     })
 
         .done(function(data) {
-          console.log(data);
+          if (data.results.length === 0) {
+            $('.all-categories').append("<p>Sorry, nothing found! Please try again.</p>");
+          }
+          else {
               var nytData = data.results;
               nytData = nytData.filter(function(item) {
                   return item.multimedia.length;
               }).splice(0, 12);
 
               nytData.forEach(function(item, index) {
-                console.log(item);
+
                   $('.all-categories').append('<div class="articlepics story-' + index + '"><div class="text"><a href="' + item.url + '"> ' + item.abstract + '</a></div></div>');
 
                   img = item.multimedia[4];
                   $('.story-' + index).css('background-image', 'url("' + img.url + '")');
                   });
-                });
+                }
+                }).always(function() {
+                  $('.loading').hide();
               });
+            });
 });
